144dafde1833d2621727cb57e018841e13b76488
ZlaKacenka <katka@zaleska.net>
2016-11-28 13:40:58 +0100

    Uprava mapy
    Zmena rozmeru mapy, zmena prikazu pro vykreslovani mapy, navazani bodu na cas, uprava sidebaru.
    Musi se opravit chybejici data, jinak to pri prehravani hazi chyby.

--- projekt.R
+++ projekt.R
@@ -16,9 +16,31 @@ data <-merge(data, souradnice, by.x="NUTS.5", by.y="Kód.obce")
 
 #vykresleni mapy CR se vsemi pokutami
 theme_set(theme_bw(16))
-CzMap <- qmap("czech republic", zoom = 7, color = "bw")
+CzMap <- qmap("czech republic", zoom = 7, color = "bw", legend="none")
 CzMap + geom_point(aes(x = Longitude, y = Latitude, colour = Zakon, size = Vyse.pokuty),
                         data = data)
 
+cz_lok <- get_googlemap("czech republic", zoom = 7, color = "bw", size = c(600, 300))
+CZMap <- ggmap(cz_lok, legend="none", extent = "device", maprange = TRUE)
+CZMap + geom_point(aes(x = Longitude, y = Latitude, colour = Zakon, size = Vyse.pokuty),
+                   data = data) + theme(legend.position = "none")
 
+ggmap(
+  get_googlemap(
+    center=c(-3.17486, 55.92284), #Long/lat of centre, or "Edinburgh"
+    zoom=14, 
+    maptype='satellite', #also hybrid/terrain/roadmap
+    scale = 2), #resolution scaling, 1 (low) or 2 (high)
+  size = c(200, 100), #size of the image to grab
+  extent='device', #can also be "normal" etc
+  darken = 0) #you can dim the map when plotting on top
 
+install.packages("leaflet")
+library(leaflet)
+
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=15.25, lat=49.59, popup="The birthplace of R")
+
+m %>% addCircles(data = data, lat = ~ Latitude, lng = ~ Longitude, radius = ~Vyse.pokuty/100, color = ~Zakon)
+m  # Print the map
