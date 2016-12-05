6ba82a79540f05b52ab0f617b4957c85788e35fc
ZlaKacenka <katka@zaleska.net>
2016-12-05 12:34:16 +0100

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,28 +1,32 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  includeCSS("styles.css"),
+  
   titlePanel("Vizualizace kontrol COI v case"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Mapa zobrazuje..."),
+      helpText("Mapa zobrazuje kontroly "),
       
       p("Puvodni data:", a("COI", href = "http://www.coi.cz/cz/spotrebitel/otevrena-data/")),
       
       sliderInput("time", 
                   label = "Mapa ke dni:",
                   timeFormat = "%d. %m. %Y",
-                  min = as.Date("2013-01-01"), max = as.Date("2015-09-09"), value = as.Date("2013-01-01"),
-                  animate = animationOptions(loop = TRUE, interval = 100)),
+                  min = as.Date("2012-01-01"), max = as.Date("2015-09-09"), value = as.Date("2012-01-01"),
+                  animate = animationOptions(loop = TRUE, interval = 1000)),
       
-      p("Legenda:"),
-      p("velikost kulicek"),
-      p("barva kulicek")
+      withTags({
+        div(class="legend",
+              p("Barevná legenda:"),
+              htmlOutput("legend_law")
+        )
+      })
     ),
     
     
-    mainPanel(plotOutput("map")#,
-              #textOutput("selected_time")
+    mainPanel(plotOutput("map", width = "100%")
     )
   )
 ))
